9547c8223dd1a682eb3e90ce246a6e571b9f0f40
Sean Chatman <136349053+seanchatmangpt@users.noreply.github.com>
2026-07-29 17:27:18 -0700

    test(verif): add D1 axiom audit surface

---
+++ verify/lean/Wasm4pmVerify/AxiomAudit.lean
@@ -0,0 +1,5 @@
+import Wasm4pmVerify.Corr.TokenReplayCounts
+
+#print axioms Wasm4pmVerify.Corr.token_replay_counts_corr
+#print axioms Wasm4pmVerify.Corr.token_replay_counts_bounds
+#print axioms Wasm4pmVerify.Corr.token_replay_counts_perfect
