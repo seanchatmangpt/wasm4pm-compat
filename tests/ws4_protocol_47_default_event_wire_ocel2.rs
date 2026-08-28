4769a950b08529f68cd73cd4f59666eff67ca79c
Sean Chatman <136349053+seanchatmangpt@users.noreply.github.com>
2026-08-27 19:37:57 -0700

    test(protocol): default capability event wire to OCEL2

---
+++ tests/ws4_protocol_47_default_event_wire_ocel2.rs
@@ -0,0 +1,3 @@
+use wasm4pm_compat::prelude::*;
+#[test]
+fn capability_defaults_to_ocel2_event_wire(){let c=CapabilityContract::new("cap","https://e/c","d","in","out",ConsequenceClass::Select,AuthorityMode::None,ReceiptPolicy::Optional,"evt");assert_eq!(c.event_wire,EventWireFormat::Ocel2);}
