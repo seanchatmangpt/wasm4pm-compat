76055c3d8eef6aecd69c510f6ec530d723b4e7ad
Sean Chatman <136349053+seanchatmangpt@users.noreply.github.com>
2026-08-27 20:38:43 -0700

    test(protocol): prove CONSTRUCT phase identity and reversibility

---
+++ tests/ws4_protocol_94_construct_phase_identity.rs
@@ -0,0 +1,3 @@
+use wasm4pm_compat::prelude::*;
+#[test]
+fn construct_intent_reports_construct_phase(){let c=CapabilityContract::new("cap","https://p","d","in","out",ConsequenceClass::Construct,AuthorityMode::None,ReceiptPolicy::Optional,"evt");let i=Intent::<ConstructPhase>::try_new(&c,SubjectRef::new("s","sd"),"id").unwrap();assert_eq!(i.consequence_class(),ConsequenceClass::Construct);assert!(i.reversible());}
