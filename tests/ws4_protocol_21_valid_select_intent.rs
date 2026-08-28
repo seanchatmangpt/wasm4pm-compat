c40c40d98d15c9e5b0d5c66e4c3c9212c50df8bd
Sean Chatman <136349053+seanchatmangpt@users.noreply.github.com>
2026-08-27 19:35:24 -0700

    test(protocol): admit valid SELECT intent

---
+++ tests/ws4_protocol_21_valid_select_intent.rs
@@ -0,0 +1,4 @@
+use wasm4pm_compat::prelude::*;
+fn c()->CapabilityContract{CapabilityContract::new("cap","https://e/c","d","in","out",ConsequenceClass::Select,AuthorityMode::None,ReceiptPolicy::Optional,"evt")}
+#[test]
+fn valid_select_intent_is_admitted(){let i=ProtocolIntent::<SelectPhase>::try_new(&c(),SubjectRef::new("s","sd"),"input").unwrap();assert_eq!(i.consequence_class(),ConsequenceClass::Select);}
