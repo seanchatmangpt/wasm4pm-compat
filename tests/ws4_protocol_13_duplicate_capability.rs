1e4cee4fbc4b3d6fcca03b287006bc41860a4880
Sean Chatman <136349053+seanchatmangpt@users.noreply.github.com>
2026-08-27 19:34:36 -0700

    test(protocol): refuse duplicate capability ids

---
+++ tests/ws4_protocol_13_duplicate_capability.rs
@@ -0,0 +1,4 @@
+use wasm4pm_compat::prelude::*;
+fn c()->CapabilityContract{CapabilityContract::new("cap","https://e/c","d","in","out",ConsequenceClass::Select,AuthorityMode::None,ReceiptPolicy::Optional,"evt")}
+#[test]
+fn duplicate_capability_id_is_refused(){let b=ProtocolBundle{protocol_id:"p".into(),version:"1".into(),capabilities:vec![c(),c()],surfaces:vec![]};assert!(b.validate().contains(&ProtocolRefusal::DuplicateCapabilityId{capability_id:"cap".into()}));}
