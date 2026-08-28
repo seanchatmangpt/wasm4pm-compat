944cadba8ac87759751f618ccccbf14a9f83061d
Sean Chatman <136349053+seanchatmangpt@users.noreply.github.com>
2026-08-27 19:36:11 -0700

    test(protocol): certify CONSTRUCT reversibility marker

---
+++ tests/ws4_protocol_29_construct_reversible.rs
@@ -0,0 +1,3 @@
+use wasm4pm_compat::prelude::*;
+#[test]
+fn construct_phase_is_reversible(){let c=CapabilityContract::new("cap","https://e/c","d","in","out",ConsequenceClass::Construct,AuthorityMode::None,ReceiptPolicy::Optional,"evt");let i=ProtocolIntent::<ConstructPhase>::try_new(&c,SubjectRef::new("s","sd"),"input").unwrap();assert!(i.reversible());}
